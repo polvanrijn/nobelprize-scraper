c9d9cbc1758f6defb7cc915f4042a3583f211c76
Pol van Rijn <pol.van.rijn@uol.de>
2017-04-19 16:04:36 +0200

    Updated the url-crawler to output the urls in the right format.

--- nobelprize/spiders/url_spider.py
+++ nobelprize/spiders/url_spider.py
@@ -3,13 +3,14 @@ from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 
 class UrlSpider(scrapy.Spider):
-    name = "url"
+    name = "url_spider"
     start_urls = [
         'http://www.nobelprize.org/nomination/archive/list.php?prize=4&year=%d' %(n) for n in range (1901, 1967)
     ]
-
+    count = 0
     def parse(self, response):
         path = response.selector.xpath('//tr//td[3]/a[contains(@href, "php")]/@href').extract()
-        yield {
-            'link': path,
-        }
+       	count =+ len(path)
+        for i in range (0, len(path)):
+	        string = "http://www.nobelprize.org/nomination/archive/" + str(response.selector.xpath('//tr//td[3]/a[contains(@href, "php")]/@href')[i].extract())
+	        print(string)
